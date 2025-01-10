6d5869929edc2586d446f9df136a7f9590aefbba
Manuela Lima Viana <49410178+Manuelalviana@users.noreply.github.com>
2025-01-10 16:43:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,17 @@ function mult() {
   var result = 
       Number(document.getElementById("firstNo").value) *
       Number(document.getElementById("secondNo").value);
-  document.getElementById("result").innerText = "Resultado da Subtração: " + result;
+  document.getElementById("result").innerText = "Resultado da Multiplicação: " + result;
   console.log(result);
 }
 function div() {
-  var result = 
-      Number(document.getElementById("firstNo").value) /
-      Number(document.getElementById("secondNo").value);
-  document.getElementById("result").innerText = "Resultado da Subtração: " + result;
-  if (secondNo === 0 {
-      document.getElementById("result").innerText = "Erro 
-)
+      var firstNo = Number(document.getElementById("firstNo").value);
+      var secondNo= Number(document.getElementById("secondNo").value);
+  if (secondNo === 0) {
+      document.getElementById("result").innerText = "Erro dividir por 0";}
+  else {
+    var result = firstNo / secundNo;
+    document.getElementById("result").innerText = "Resultado da Divisão: " + result;
+  }
 }
+  //// 
