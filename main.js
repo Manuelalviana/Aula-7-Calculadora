ff91587bae1a3e6a2bf1930ddb3f0f200ab4555a
Manuela Lima Viana <49410178+Manuelalviana@users.noreply.github.com>
2025-01-10 15:38:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function add() {
+  var result = 
+      Number(document.getElementById("firstNo").value) +
+      Number(document.getElementById("secondNo").value);
+  document.getElementById("result").innerText = "Resultado da Adição: " + result;
+}
+
+function sub() {
+  var result = 
+      Number(document.getElementById("firstNo").value) -
+      Number(document.getElementById("secondNo").value);
+  document.getElementById("result").innerText = "Resultado da Subtração: " + result;
+}
